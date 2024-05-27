37bfc0d9e09faafcc2cd2813dadd52260b116313
Student <Student@B111-13>
2024-05-27 20:08:43 +0300

    Add project files.

---
+++ FinalSolution.cpp
@@ -0,0 +1,22 @@
+/** \file FinalSolution.cpp
+* \brief A program for a final exam 
+* \author Oleg Rubenchik
+* \data 27/05/2024
+*/
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+//GLOBAL VARIABLES
+
+//FUNCTION PROTOTYPES
+
+
+//MAIN
+int main() {
+
+
+	return 0;
+}
+
+//FUNCTIONS
